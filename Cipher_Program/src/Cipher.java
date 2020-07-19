2eaabbe945dc9e3ad498464b56be373a37e8f60c
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-07-18 20:14:59 -0600

    Update Cipher.java

--- Cipher_Program/src/Cipher.java
+++ Cipher_Program/src/Cipher.java
@@ -18,12 +18,13 @@ public class Cipher {
 		Total_1, Total_2, //Variables for arithmetic operations
 		encodedNumber1, encodedNumber2; //Variables corresponding to tens and ones digits
 	
+	// - PROGRAM BODY - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	System.out.println("Welcome to the Cipher Program.");
 	System.out.println("Please enter five digits between 0 to 19");
 	Scanner userNumber = new Scanner(System.in);
 	
-	// - USER INPUT #1 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	// - - USER INPUT #1 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	System.out.println("1st number: " + (user1 = userNumber.nextInt()));
 		if (user1 < 0); {
 		System.out.println("Please read directions and try again!");
@@ -33,7 +34,7 @@ public class Cipher {
 		System.out.println("Please read directions and try again!");
 		System.exit(0);}
 	
-	// - USER INPUT #2 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	// - - USER INPUT #2 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	System.out.println("2nd number: " + (user2 = userNumber.nextInt()));
 		if (user2 < 0) {
 		System.out.println("Please read directions and try again!");
@@ -43,7 +44,7 @@ public class Cipher {
 		System.out.println("Please read directions and try again!");
 		System.exit(0);}
 	
-	// - USER INPUT #3 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	// - - USER INPUT #3 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	System.out.println("3rd number: " + (user3 = userNumber.nextInt()));
 		if (user3 < 0) {
 		System.out.println("Please read directions and try again!");
@@ -53,7 +54,7 @@ public class Cipher {
 		System.out.println("Please read directions and try again!");
 		System.exit(0);}
 	
-	// - USER INPUT #4 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	// - - USER INPUT #4 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	System.out.println("4th number: " + (user4 = userNumber.nextInt()));
 		if (user4 < 0) {
 		System.out.println("Please read directions and try again!");
@@ -63,7 +64,7 @@ public class Cipher {
 		System.out.println("Please read directions and try again!");
 		System.exit(0);}
 	
-	// - USER INPUT #5 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	// - - USER INPUT #5 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	System.out.println("5th number: " + (user5 = userNumber.nextInt()));	
 		if (user5 < 0) {
 		System.out.println("Please read directions and try again!");
@@ -73,15 +74,14 @@ public class Cipher {
 		System.out.println("Please read directions and try again!");
 		System.exit(0);}
 	
-	//Generates the plaintext number for the Modulo 10
+	// - - Generates the plaintext number and random key for the Modulo 10
 	Total = user1 + user2 + user3 + user4 + user5;
 	System.out.println("Total: " + Total);
 	
-	//Generates random key number for encoding
 	Random key = new Random();
 	System.out.println("Your random key is " + (randomKey = key.nextInt(10)));
 	
-	// MODULO 10 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	// - - MODULO 10 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	// Modulo 10 on plain text number comprising: 
 	// conditional statements generating the encoded number,
 	// in both cases of the total being < 10 or >= 10. 
@@ -97,15 +97,15 @@ public class Cipher {
 		encodedNumber1 = ((Total / 10) + randomKey)%10; //Total / 10 gives tens digit
 		Total_2 = (Total / 10) * 10;  //Gives access to ones place for encodednumber2
 		encodedNumber2 = ((Total - Total_2) + randomKey)%10;
-		// Checks to ensure that the right values of the individual encoded digits are being calculated
-		// System.out.println("Encoded number 1: " + (Total / 10));
-		// System.out.println("Total_2: " + Total_2);
-		// System.out.println("Encoded number 2: " + (Total - Total_2));
+			// Check-system code to ensure that the right values of the individual encoded digits are being calculated
+			// System.out.println("Encoded number 1: " + (Total / 10));
+			// System.out.println("Total_2: " + Total_2);
+			// System.out.println("Encoded number 2: " + (Total - Total_2));
 		System.out.println("Your encoded number is " + encodedNumber1 + encodedNumber2);}
 	
-
-	userNumber.close();
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
+	userNumber.close();
 	}
 
 }
