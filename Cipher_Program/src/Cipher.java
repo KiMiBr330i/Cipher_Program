90633f8750aeda6de6c8a0513699979735d7ef68
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-07-19 19:15:06 -0600

    Update Cipher.java

--- Cipher_Program/src/Cipher.java
+++ Cipher_Program/src/Cipher.java
@@ -26,53 +26,63 @@ public class Cipher {
 	
 	// - - USER INPUT #1 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	System.out.println("1st number: " + (user1 = userNumber.nextInt()));
-		if (user1 < 0); {
+		if (user1 < 0) {
 		System.out.println("Please read directions and try again!");
-		System.exit(0);}
+		System.exit(0);
+		}
 		
 		if (user1 > 19) {
 		System.out.println("Please read directions and try again!");
-		System.exit(0);}
+		System.exit(0);
+		}
 	
 	// - - USER INPUT #2 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	System.out.println("2nd number: " + (user2 = userNumber.nextInt()));
 		if (user2 < 0) {
 		System.out.println("Please read directions and try again!");
-		System.exit(0);}
+		System.exit(0);
+		}
 		
 		if (user2 > 19) {
 		System.out.println("Please read directions and try again!");
-		System.exit(0);}
+		System.exit(0);
+		}
 	
 	// - - USER INPUT #3 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	System.out.println("3rd number: " + (user3 = userNumber.nextInt()));
 		if (user3 < 0) {
 		System.out.println("Please read directions and try again!");
-		System.exit(0);}
+		System.exit(0);
+		}
 		
 		if (user3 > 19) {
 		System.out.println("Please read directions and try again!");
-		System.exit(0);}
+		System.exit(0);
+		}
 	
 	// - - USER INPUT #4 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	System.out.println("4th number: " + (user4 = userNumber.nextInt()));
 		if (user4 < 0) {
 		System.out.println("Please read directions and try again!");
-		System.exit(0);}
+		System.exit(0);
+		}
 		
 		if (user4 > 19) {
 		System.out.println("Please read directions and try again!");
-		System.exit(0);}
+		System.exit(0);
+		}
 	
 	// - - USER INPUT #5 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 	System.out.println("5th number: " + (user5 = userNumber.nextInt()));	
 		if (user5 < 0) {
 		System.out.println("Please read directions and try again!");
-		System.exit(0);}
+		System.exit(0);
+		}
 		
 		if (user5 > 19) {
 		System.out.println("Please read directions and try again!");
-		System.exit(0);}
+		System.exit(0);
+		}
 	
 	// - - Generates the plaintext number and random key for the Modulo 10
 	Total = user1 + user2 + user3 + user4 + user5;
